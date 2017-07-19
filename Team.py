64e8e4841fa0abfd1c659ac4971a8549bde2c0c4
steventhanna <steventhanna@gmail.com>
2017-07-19 09:46:38 -0500

    Basic upload and download of a team

--- Team.py
+++ Team.py
@@ -26,32 +26,68 @@ class Team(object):
     # @param :: logs - an array of log id's in association with our API
     # @end
     def __init__(self, apiID, name, city, tricode, teamID, players, logo, seasonWins, seasonLosses, location, logs):
-        self.id = apiID
+        # self.id = apiID
         self.name = name
         self.city = city
         self.tricode = tricode
-        self.teamID = teamID
         self.players = players
         self.logo = logo
+        self.teamID = teamID
         self.seasonWins = seasonWins
         self.seasonLosses = seasonLosses
         self.location = location
         self.logs = logs
+        # Try to get an ID
+        if apiID == None:
+            result = self.get_api_id()
+            if result != None:
+                self.id = result
 
     def upload(self):
         x = json.dumps(self, default=lambda x: x.__dict__)
         print(x)
 
+    # @type :: FUNC
+    # @name :: json_dump
+    # @param :: self - self representation
+    # @description :: Serialzes an object and dumps the result into JSON
+    # @return :: A JSON representation of a team object
+    # @end
+    def json_dump(self):
+        return json.dumps(self, default=lambda x: x.__dict__)
+
+    # @type :: FUNC
+    # @name :: upload_new
+    # @param :: self - self representation
+    # @description :: Uploads a new team
+    # @end
+    def upload_new(self):
+        r = requests.post('https://therender-nba-api.herokuapp.com/team/new', data=self.json_dump())
+
+    # @type :: FUNC
+    # @name :: upload_existing
+    # @param :: self - self representation
+    # @description :: Uploads an existing team as an edit
+    # @end
+    def upload_existing(self):
+        r = requests.post('https://therender-nba-api.herokuapp.com/team/edit', data=self.json_dump())
+
+    # @type :: FUNC
+    # @name :: get_api_id
+    # @param :: self
+    # @returns :: False | the referenced API ID
+    # @description :: Determines if the team exists or not. If it does, it
+    # supplies the API id
+    # @end
     def get_api_id(self):
         # r = requests.get('https://nba-api.therendersports.com/team/exists/nbaid/' + self.teamID)
         r = requests.get('https://therender-nba-api.herokuapp.com/team/exists/nbaid/' + self.teamID)
         # r = requests.get('https://therender-nba-api.herokuapp.com/teams')
+        r = r.json()
         if r["exists"] == False:
-            print("FALSE")
+            return None
         else:
-            print("TRUE")
-        print(r.json())
+            return r["id"]
 
-
-x = Team("123", "Test", "Test", "Test", "Test", ["Test"], "Test", "Test", "Test", "Test", ["Test"])
-x.get_api_id()
+x = Team(None, "Test", "Test", "Test", "Test", ["Test"], "http://google.com", 0, 0, "Test", ["Test"])
+# x.upload_new()
